31325eea38d7eb4ffadefeb09c256cfa9d671550
Christopher Stull <cjstull33@gmail.com>
2020-11-20 09:50:39 -0600

    added form validation js as bonus funtionality

--- js/contactFormValidator.js
+++ js/contactFormValidator.js
@@ -1,34 +1,37 @@
-function isFieldEmpty () {
-    const field = document.querySelector('#');
-    if (field.value === "") {
-        return true;
-    } else {
-        return false;
+
+
+var form = document.getElementById("contactForm");
+
+
+/*listen for submit button, execute the following*/
+form.addEventListener("submit", (e) => {
+    /*grab the input in the name, email, and phone field and store as variables*/
+    var name = document.getElementById("name");
+    var email = document.getElementById("mail");
+    var phone = document.getElementById("phone");
+    var comments = document.getElementById("comments");
+    var errorElement = document.getElementById("error-message");
+  
+    var emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let messages = [];
+    /*test that name is not empty*/
+    if(name.value == null || name.value == "" ){
+        messages.push('Name field cannot be left blank');
     }
-}
-
-const fieldTest = isFieldEmpty();
-
-if (fieldTest === true) {
-    alert('Please provide your information')
-}
-
-/*grab the input in the name, email, and phone field and store as variables*/
-var name = document.getElementById("name");
-var email = document.getElementById("mail");
-var phone = document.getElementById("phone");
-
-function validateForm() {
-    if ( !name.value ) {
-    alert("Enter Name.");
-    name.focus();
-    } else if ( !email.value ) {
-    alert("Enter email.");
-    email.focus();
-    } else if ( !phone.value ) {
-    alert("Enter phone number.");
-    phone.focus();
+    if(!(email.value.match(emailRegex))){
+        messages.push('Enter valid email address');
     }
- }
+    if(comments.length == null){
+        messages.push('Comment field cannot be left blank');
+    }
 
- submitButton.addEventListener("click", validateForm());
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+    else{
+        alert(`Thank you, someone will be in contact shortly. ${name.value}`);
+        
+    }
+}  
+); /*?closeEventListener*/
